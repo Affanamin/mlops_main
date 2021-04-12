ebfac224756b59c91d76b1b7996bc0ee8080b2a7
unknown <affanaminn@gmail.com>
2021-04-12 16:00:46 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its an end to end wafer detection project using MLOPS for the first time',
+    author='affan',
+    license='MIT',
+)
